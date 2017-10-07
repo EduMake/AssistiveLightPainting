c7858318f6c505653e87a2295eb6e49d97958d00
Martyn Eggleton <martyn.eggleton@gmail.com>
2017-10-07 19:49:51 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+var DOMParser = require("domparser");
 var linearize = require('svg-linearize');
 var svg = require("svg")
 var fs = require('fs');
 
+
 text = fs.readFileSync("Fish.svg", "utf-8")
 var elem = svg(text)
 
-//var loadsvg = require('load-svg');
+var loadsvg = require('load-svg');
 
-//loadsvg('Fish.svg', function (err, svg) {
-    //var nsvg = linearize(svg, { tolerance: 3 });
+loadsvg('Fish.svg', function (err, svg) {
+    var nsvg = linearize(svg, { tolerance: 3 });
     
-    var nsvg = linearize(elem, { tolerance: 3 });
+    //var nsvg = linearize(elem, { tolerance: 3 });
     fs.writeFileSync("Fish2.svg", nsvg)
-//});
+});
