5749e5456fd62a1d17ae4a03fea0261ceaa3c873
Martyn Eggleton <martyn.eggleton@gmail.com>
2017-10-08 18:16:26 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var fs = require('fs');
-var DOMParser = require("domparser");
+var DOMParser = require('xmldom').DOMParser;
+var XMLSerializer = require('xmldom').XMLSerializer;
 
-var svg = require("svg");
+var doc = new DOMParser().parseFromString(
+    '<xml xmlns="a" xmlns:c="./lite">\n'+
+        '\t<child>test</child>\n'+
+        '\t<child></child>\n'+
+        '\t<child/>\n'+
+    '</xml>'
+    ,'text/xml');
+    
+
+var nsvg = XMLSerializer.serializeToString(doc);
+fs.writeFileSync("Fish2.svg", nsvg);
+
 //var linearize = require('svg-linearize');
 
 
 
-
+/*
 text = fs.readFileSync("Fish.svg", "utf-8");
 var elem = svg(text);
 
 console.log(elem);
-/*
+
 var loadsvg = require('load-svg');
 
 loadsvg('Fish.svg', function (err, svg) {
